776c20a1699026192c836facb46839f042d1c29b
Emil Albin Ryberg <eryber17@student.aau.dk>
2020-11-13 11:05:00 +0100

    Added message types

--- src/person_detector/person_detector/person_detector_node.py
+++ src/person_detector/person_detector/person_detector_node.py
@@ -1,9 +1,12 @@
 import rclpy
 from rclpy.node import Node
 from rcl_interfaces.msg import ParameterType
+from sensor_msgs.msg import CompressedImage
+from person_follower_interfaces.msg import PersonInfo, PersonInfoList
 import numpy
 import torch
 import os
+from cv_bridge import CvBridge
 from person_detector.feature_extractor_module.feature_extractor import FeatureExtractor
 
 from std_msgs.msg import String
@@ -11,24 +14,19 @@ from std_msgs.msg import String
 
 class PersonDetector(Node):
     def __init__(self):
-        super().__init__('person_detector')
+        super().__init__("person_detector")
         self.declare_parameter("feature_weights_path")
         feature_weights_path = self.get_parameter("feature_weights_path").get_parameter_value().string_value
         feature_weights_path = os.path.expanduser(feature_weights_path)
-        print(f"Weights path: {feature_weights_path}")
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.image_subscriber = self.create_subscription(CompressedImage,
+                                                         "/xtion/rgb/image_raw/compressed",
+                                                         self.image_callback,
+                                                         1)
         self.fe = FeatureExtractor(feature_weights_path, on_gpu=False)
+        self.cv_bridge = CvBridge()
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
-
+    def image_callback(self, msg):
+        image = self.cv_bridge.compressed_imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
 def main(args=None):
     rclpy.init(args=args)
