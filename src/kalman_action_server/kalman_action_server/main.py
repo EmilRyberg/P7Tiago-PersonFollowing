b339b18c21eb589301a2e8a47ad7dd5726e2a144
Benjamin <benjaminhoej@hotmail.com>
2020-11-17 12:09:52 +0100

    Implemented result only action server

--- src/kalman_action_server/kalman_action_server/main.py
+++ src/kalman_action_server/kalman_action_server/main.py
@@ -1,11 +1,12 @@
 import rclpy
-from person_follower_interfaces.msg import *
+from person_follower_interfaces.msg import PersonInfoList
 from person_follower_interfaces.action import *
 from kalman_action_server.ownKalman import KfTracker
 from rclpy.action import ActionServer
 from rclpy.node import Node
 import numpy as np
 
+# Used for sorting the list from the vision module
 def sort_obj_based_on_id(obj):
     return obj.person_id
 
@@ -14,19 +15,22 @@ class KalmanTracking(Node):
     kf = list()
     kf_number = 0
 
+    tracked_id = -1
+
     def __init__(self):
 
         super().__init__('kalman_action_server')
-        self.subscription = self.create_subscription(PersonInfoList, 'topic', self.detection_cb, 1)
+        self.subscription = self.create_subscription(PersonInfoList, '/persons', self.detection_cb, 1)
 
         self._action_server = ActionServer(self, Kalman, 'kalmanTracker', self.action_cb)
 
     def detection_cb(self, msg):
         time = msg.header.timestamp.sec + msg.header.timestamp.nanosec / 1e9 # Conversion to seconds
         persons = msg.persons.persons
-        persons.sort(key=sort_obj_based_on_id)
+        persons.sort(key=sort_obj_based_on_id) # Sorting based on ID
+        self.tracked_id = msg.tracked_id
 
-        if len(persons) == 0:
+        if len(persons) == 0: # If no new IDs have come, these are simply updated
             for i in range(self.kf_number):
                 self.kf[i].predict(time)
                 self.kf[i].isTracked = False
@@ -46,8 +50,8 @@ class KalmanTracking(Node):
                 self.kf[i].predict(time)
 
                 # Getting the measured position into the right format
-                mPos = np.array([[persons[newPersonIdx].point.x], 
-                                 [persons[newPersonIdx].point.y]])
+                mPos = np.array([persons[newPersonIdx].point.x, 
+                                 persons[newPersonIdx].point.y])
 
                 self.kf[i].update(mPos)
                 
@@ -57,8 +61,8 @@ class KalmanTracking(Node):
             # When a new ID has come
             else:
                 # Getting the measured position into the right format
-                mPos = np.array([[persons[newPersonIdx].point.x], 
-                                 [persons[newPersonIdx].point.y]])
+                mPos = np.array([persons[newPersonIdx].point.x, 
+                                 persons[newPersonIdx].point.y])
 
                 self.kf.append(KfTracker(mPos, time))
                 self.kf_number += 1
@@ -66,29 +70,21 @@ class KalmanTracking(Node):
                 newPersonIdx += 1
 
     def action_cb(self, cb_handle):
-        print("Executing goal...")
-
         id = cb_handle.request.id
 
-        feedback_msg = Kalman.Feedback()
+        cb_handle.succeed() # Saying the goal was accomplished
 
-        while True:
-            feedback_msg.point.x = self.kf[id].x[0, 0]
-            feedback_msg.point.y = self.kf[id].x[1, 0]
-            feedback_msg.is_found = self.kf[id].isTracked
+        result = Kalman.Result() # Creating result message
+        
+        if self.kf_number != 0: # Making sure atleast one kalman filter is running before indexing
+            result.point.x = self.kf[id].x[0, 0]
+            result.point.y = self.kf[id].x[1, 0]
+            result.is_tracked = self.kf[id].isTracked
 
-            cb_handle.publish_feedback(feedback_msg)
-            time.sleep(1)
+        result.tracked_id = self.tracked_id # Replying which ID is to be tracked, essentially forwarding from earlier
 
-        cb_handle.succeed()
-
-        result = Kalman.Result()
-        result.is_tracked = False#self.kf[]
         return result
 
-
-
-import time
 
 def main():
     rclpy.init()
