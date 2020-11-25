bounding box limit fix and added depth averaging

src/person_detector/person_detector/person_detector_node.py
@@ -229,14 +229,7 @@ class PersonDetector(Node):
         # Here, if we have a bounding box, we find the position of the object and publish it
         # Getting the middle pixel of the bounding box
         # TODO replace with better method
-        xp = int(bounding_box[0]) + int(bounding_box[2] / 2)
-        yp = int(bounding_box[1]) + int(bounding_box[3] / 2)
-
-        # The middle pixel can be outside the frame, so we stop that
-        xp = W - 1 if xp >= W else xp
-        xp = 0 if xp < 0 else xp
-        yp = H - 1 if yp >= H else yp
-        yp = 0 if yp < 0 else yp
+        dist = self.read_depth(self.depth_image, bounding_box)
 
         # Getting the distance to the target
         dist = self.depth_image.item(yp, xp)
@@ -294,6 +287,25 @@ class PersonDetector(Node):
                [[ 1.0-(yY+zZ), xY-wZ, xZ+wY ],
                 [ xY+wZ, 1.0-(xX+zZ), yZ-wX ],
                 [ xZ-wY, yZ+wX, 1.0-(xX+yY) ]])
+
+    def read_depth(self, dImg, bbox):
+        centerx=int((bbox[0]+bbox[2])/2)
+        centery=int((bbox[1]+bbox[3])/2)
+        x1 = centerx - 10
+        y1 = centery - 10
+        x2 = centerx + 10
+        y2 = centery + 10
+        count = 0
+        dist = 0
+        while x1 <= x2:
+            while y1 <= y2:
+                if not np.isnan(dImg.item(y1, x1)):
+                    dist = dist + dImg.item(y1, x1)
+                    count+=1
+                y1 = y1 + 1
+            x1 = x1 + 1
+        average_depth = dist / count
+        return average_depth
 
 
 def main(args=None):

src/person_detector/person_detector/person_finder/person_finder.py
@@ -30,10 +30,12 @@ class PersonFinder:
         x1, y1, x2, y2, conf, cls_conf, cls_pred = detection
         box_w = x2 - x1
         box_h = y2 - y1
+        img_h = np_image.shape[0]
+        img_w = np_image.shape[1]
         ix1 = max(0, int(x1))
-        ix2 = max(0, int(x2))
+        ix2 = min(img_w-1, int(x2))
         iy1 = max(0, int(y1))
-        iy2 = max(0, int(y2))
+        iy2 = min(img_h-1, int(y2))
         cropped_img = np.array(np_image[iy1:iy2, ix1:ix2, :])
         return cropped_img
 
@@ -49,7 +51,13 @@ class PersonFinder:
         detections = detections[0]
         if detections is not None:
             detections = rescale_boxes(detections, 416, np_image.shape[:2])
+            for detection in detections:
+                detection[0] = max(0, int(detection[0]))
+                detection[1] = max(0, int(detection[1]))
+                detection[2] = min(np_image.shape[1], int(detection[2]))
+                detection[3] = min(np_image.shape[0], int(detection[3]))
             person_dets = [det for det in detections if int(det[6]) == 0]  # Detection of ID 0 is person
+
             return person_dets
         else:
             return []